513e9c21181b55ca8f6f4e4a3f7a0fcb5903d3d9
kang zhang <jackbobswift@gmail.com>
2024-06-21 19:43:55 +0800

    initial scripts

---
+++ build_env/create.sh
@@ -0,0 +1,11 @@
+echo "Dist Root: ${DIST_ROOT:?}"
+echo "LFS: ${LFS:?}"
+
+if ! test $(whoami) == "distbuild" ; then
+    echo "Must run as distbuild!"
+    exit -1
+fi
+
+echo "Creating build environment..."
+
+
